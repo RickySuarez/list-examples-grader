c517a84f95587ecb02deac13fffad971b943a823
RickySuarez <rick.suarez1994@gmail.com>
2022-11-16 08:28:45 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,7 +1,24 @@
-# Create your grading script here
-
 set -e
-
+FILE="ListExamples.java"
+CPATH=".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
 rm -rf student-submission
 git clone $1 student-submission
-
+cd student-submission
+if [[ -f "$FILE" ]]
+then
+        echo "$FILE has been found."
+else
+        echo "$FILE was not found."
+        exit
+fi
+cd ../
+cp TestListExamples.java student-submission/
+cp -r lib student-submission/
+cd student-submission/
+javac -cp $CPATH *.java
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples | grep "Tests run:" > results.txt
+NUMS=(`grep -o '[0-9]' results.txt`)
+PASS=${NUMS[0]}
+TOTAL=${NUMS[1]}
+echo $PASS
+echo $TOTAL
